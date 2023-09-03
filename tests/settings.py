659fa9b1700718d07dc2f49bf70424e062007e6a
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2023-09-03 00:25:58 -0300

    issue #1: initial setup for package

--- tests/settings.py
+++ tests/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'djmanhwabookmarks',
-    
+
 
     # if your app has other dependencies that need to be added to the site
     # they should be added here
