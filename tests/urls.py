45d794de03223ee1f3b2aa8ac5451dd0c2d5a045
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2023-11-12 02:10:07 -0300

    issue #24: redirect index to /admin

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     path('', include('djmanhwabookmarks.urls', namespace='djmanhwabookmarks')),
 ]
