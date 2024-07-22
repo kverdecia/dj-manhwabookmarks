1d50fbb2d7150c5252e5d52e13df4458166ffde2
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2024-07-22 05:34:52 -0300

    issue #49: add support for social login

--- tests/urls.py
+++ tests/urls.py
@@ -5,8 +5,15 @@ from django.urls import path, include, reverse_lazy
 from django.contrib import admin
 from django.views.generic import RedirectView
 
+from allauth.account.decorators import secure_admin_login
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     path('', include('djmanhwabookmarks.urls', namespace='djmanhwabookmarks')),
 ]
+
+admin.autodiscover()
+admin.site.login = secure_admin_login(admin.site.login)
